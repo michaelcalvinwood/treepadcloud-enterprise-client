a25b82a7f01221bd14d8b8d1d4dbbdb6c184a4f5
Michael Wood <michaelwood33311@icloud.com>
2023-02-12 11:01:36 -0500

    added treepad cloud icon and title to initial mobile page

--- src/mobilePages/GoodMemories.tsx
+++ src/mobilePages/GoodMemories.tsx
@@ -1,8 +1,8 @@
-import { IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar, isPlatform } from "@ionic/react";
+import { IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonImg, IonPage, IonRow, IonTitle, IonToolbar, isPlatform } from "@ionic/react";
 import React, { useContext } from "react";
 
 import { add } from 'ionicons/icons';
-
+import treePadIcon from '../assets/icons/treepadcloud-icon.svg';
 
 const GoodMemories: React.FC = () => {
    
@@ -10,8 +10,19 @@ const GoodMemories: React.FC = () => {
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>
-                        Good Memories
+                    
+                    <IonTitle className="ion-text-center" style={{verticalAlign: "middle"}}>
+                        {/* <IonImg src={treePadIcon} style={{height: "1.5rem", display: "inline-block"}} />
+                        <div style={{lineHeight: "1.5rem", verticalAlign: "middle", display: "inline-block"}}>TreePad Cloud</div> */}
+
+                        <IonRow className="ion-align-items-center">
+                            <IonCol size="6">
+                                <img src={treePadIcon} style={{height: "1.5rem"}} className="ion-float-right"/>
+                            </IonCol>
+                            <IonCol className="title-col" size="6" >
+                                <div className="ion-float-left">TreePad Cloud</div>
+                            </IonCol>
+                        </IonRow>
                     </IonTitle>
                     {/* {isPlatform('ios') && (
                         <IonButtons slot="end">
