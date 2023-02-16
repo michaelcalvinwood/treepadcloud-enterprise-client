7a9292fed45625c9fe25b449baa51b6f8ffd9573
Michael Wood <michaelwood33311@icloud.com>
2023-02-15 19:57:43 -0500

    refactored icons list to remove .svg for sorting purposes

--- src/data/createIconFile.js
+++ src/data/createIconFile.js
@@ -4,6 +4,8 @@ let fileNames = fs.readFileSync('svg-filenames.json', 'utf8');
 fileNames = JSON.parse(fileNames);
 let icons = fileNames.map(file => {
     let parts = file.split('/');
+    let loc = parts[3].indexOf('.');
+    parts[3] = parts[3].substr(0, loc);
     return {
         t: parts[2],
         n: parts[3]
